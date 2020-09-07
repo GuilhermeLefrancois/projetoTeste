610bea0ce0082c549bb111ff09d1cb0f13ae7b9f
Guilherme Lefrancois <gui.lefrancois21@gmail.com>
2020-09-07 16:59:03 -0300

    commit arquivo teste.py'

---
+++ teste.py
@@ -0,0 +1,9 @@
+def funcao():
+    idade = int(input('Digite sua idade: '))
+    if(idade >= 18):
+        print('E maior de idade')
+    else:
+	    print('E menor de idade')
+
+if(__name__ == '__main__'):
+    funcao()
